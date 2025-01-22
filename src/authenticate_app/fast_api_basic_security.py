7d389c7136c66f93a6e16e8f7d9fa091dd11e783
anliwi <alwirth@posteo.de>
2025-01-22 18:00:28 +0100

    Improved API

--- src/authenticate_app/fast_api_basic_security.py
+++ src/authenticate_app/fast_api_basic_security.py
@@ -8,34 +8,33 @@ app = FastAPI()
 security = HTTPBasic()
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+# Users dictionary with hashed passwords
 users = {
-
     "admin1": {
         "username": "admin1",
         "name": "Admin",
         "hashed_password": pwd_context.hash('1nimda'),
     },
-
-    "user1" : {
-        "username" :  "user1",
-        "name" : "User",
-        "hashed_password" : pwd_context.hash('1resu'),
+    "user1": {
+        "username": "user1",
+        "name": "User",
+        "hashed_password": pwd_context.hash('1resu'),
     }
-
 }
 
+# Function to verify user credentials
 def get_current_user(credentials: HTTPBasicCredentials = Depends(security)):
     username = credentials.username
-    if not(users.get(username)) or not(pwd_context.verify(credentials.password, users[username]['hashed_password'])):
+    if username not in users or not pwd_context.verify(credentials.password, users[username]['hashed_password']):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect email or password",
+            detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Basic"},
         )
-    return credentials.username
+    return username
 
 @app.get("/user")
 def current_user(username: str = Depends(get_current_user)):
-    # defining redirection
-    redirect_url = "https://rpubs.com/helenabakic/women-in-open-data"  
+    # If the user is authenticated, redirect to a different URL
+    redirect_url = "https://rpubs.com/helenabakic/women-in-open-data"
     return RedirectResponse(url=redirect_url)
